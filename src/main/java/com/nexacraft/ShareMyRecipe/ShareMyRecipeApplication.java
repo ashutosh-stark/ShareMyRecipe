ec45861963e50637e06477be237ad8c56e96a431
ashutosh3.pandey <ashutosh3.pandey@ril.com>
2025-11-08 13:15:44 +0530

    intial push

---
+++ src/main/java/com/nexacraft/ShareMyRecipe/ShareMyRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.nexacraft.ShareMyRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMyRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMyRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexacraft/ShareMyRecipe/ShareMyRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexacraft.ShareMyRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMyRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
